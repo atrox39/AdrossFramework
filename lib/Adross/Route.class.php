a8a94a904768f3316942c0f6abb455783c768876
atrox39 <cofter11@gmail.com>
2021-05-12 20:49:03 -0700

    Adding send and json function in request

--- lib/Adross/Route.class.php
+++ lib/Adross/Route.class.php
@@ -10,4 +10,16 @@ class Route
         $path = "views/".$view.".html";
         echo new Template($path, $context);
     }
+
+    public function send($text)
+    {
+        header('Content-Type:text/plain');
+        echo $text;
+    }
+
+    public function json($json)
+    {
+        header('Content-Type:application/json');
+        echo json_encode($json);
+    }
 }
