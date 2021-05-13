45551d2138ebd72742392e41ecf1def103baedde
atrox39 <cofter11@gmail.com>
2021-05-12 20:18:18 -0700

    new command in manage.php, rebuild_all working now

--- manage.php
+++ manage.php
@@ -1,12 +1,19 @@
 <?php
 include_once('./autoload.class.php');
-use Adross\App;
 use Adross\Config;
 if(isset($argv[1]))
 {
     switch($argv[1]){
         case "rebuild_all":
-            
+            $c = 0;
+            foreach (scandir('models') as $file) {
+                if ($file != ".." && $file != "." && $file != "Base.model.php") {
+                    $file = explode(".", $file);
+                    new $file[0](true);
+                    echo "\t\e[36m- \e[33mModelname\e[35m: \e[39m" . $file[0] . "\n\n";
+                    $c += 1;
+                }
+            }
         break;
 
         case "models":
